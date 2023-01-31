43f2b729ef2a1fbc507bf418464d1ced852eec9e
Shenthamarai Selvi <shenthamarai.n@gmail.com>
2023-01-31 15:50:41 -0500

    adding custom filter

--- gateway/src/main/java/org/example/config/SpringCloudConfig.java
+++ gateway/src/main/java/org/example/config/SpringCloudConfig.java
@@ -1,15 +1,20 @@
 package org.example.config;
 
+import org.example.demo.MyCustomFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.example.demo.MyCustomFilter.Config;
+
 @Configuration
 public class SpringCloudConfig {
     @Bean
-    public RouteLocator buildRouteLocator(RouteLocatorBuilder builder){
-        return builder.routes().route( r -> r.path("/price/**").uri("http://localhost:8002"))
+    public RouteLocator buildRouteLocator(RouteLocatorBuilder builder, MyCustomFilter myCustomFilter){
+        return builder.routes().route( r -> r.path("/price/**")
+                        .filters(f -> f.filter( myCustomFilter.apply(new Config())) )
+                        .uri( "http://localhost:8002"))
                         .route( r -> r.path( "/inventory/**").uri( "http://localhost:8005")).build();
     }
 }
---
+++ gateway/src/main/java/org/example/demo/MyCustomFilter.java
@@ -0,0 +1,64 @@
+package org.example.demo;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+@Component
+public class MyCustomFilter extends AbstractGatewayFilterFactory<MyCustomFilter.Config> {
+    public MyCustomFilter() {
+        super(Config.class);
+    }
+
+    private boolean isAuthorizationValid(String header) {
+        // Code to check auth
+        return true;
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.setComplete();
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey("Authorization")) {
+                return this.onError(exchange, "No Authorization header found", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get("Authorization").get(0);
+
+            if (!this.isAuthorizationValid(authorizationHeader)) {
+                return this.onError(exchange, "Invalid Authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().path("/price/103").build();
+
+            //Pre-Filter
+            //return chain.filter(exchange.mutate().request(modifiedRequest).build());
+
+            //Post-Filter
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("Post filter"); }));
+
+
+        };
+    }
+
+    public static class Config {
+        // Config properties
+    }
+}
